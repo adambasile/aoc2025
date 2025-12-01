fad55b5006d58ee70226f4517ce977bbc7a63247
Adam Basile <basile.adam@gmail.com>
2025-12-01 15:59:45 +1100

    Add scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::Parser;
+use std::fmt::{Display, Formatter};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+use std::time::Instant;
+
+#[derive(Parser)]
+struct Cli {
+    day: i64,
+    path: PathBuf,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum FunctionOutput {
+    IntPair(i64, i64),
+    StringPair(String, String),
+}
+
+impl Display for FunctionOutput {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FunctionOutput::IntPair(a, b) => write!(f, "{}, {}", a, b),
+            FunctionOutput::StringPair(a, b) => write!(f, "{}, {}", a, b),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Cli = Cli::parse();
+    let lines = read_lines_from_file(args.path);
+    let before = Instant::now();
+    let day: fn(Vec<String>) -> FunctionOutput = match args.day {
+        _ => panic!(),
+    };
+    println!("{}", day(lines));
+    println!("Elapsed time: {:.2?}", before.elapsed());
+}
+
+fn read_lines_from_file(path: PathBuf) -> Vec<String> {
+    let file = File::open(path).unwrap();
+    let lines: Vec<String> = BufReader::new(file)
+        .lines()
+        .filter_map(|result| result.ok())
+        .collect();
+    lines
+}
+
+#[allow(unused)]
+fn read_testfile(testfile: &str) -> Vec<String> {
+    let filename = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("inputs")
+        .join(testfile);
+    let lines = read_lines_from_file(filename);
+    lines
 }
