b28d47380a5860845e4a876f1f602f34a087d8fb
Adam Basile <basile.adam@gmail.com>
2025-12-03 17:17:21 +1100

    Day 3 part two done

--- src/day03.rs
+++ src/day03.rs
@@ -1,36 +1,55 @@
 use crate::FunctionOutput;
 use crate::FunctionOutput::IntPair;
 
-struct BatteryBank(Vec<i64>);
+type BatteryBank = Vec<i64>;
 
-impl BatteryBank {
-    fn max_joltage(&self) -> i64 {
-        let first_digit = self.0[..self.0.len() - 1].iter().max().unwrap();
-        let first_digit_idx = self.0.iter().position(|i| i == first_digit).unwrap();
-        let second_digit = self.0[first_digit_idx + 1..].iter().max().unwrap();
-
-        first_digit * 10 + second_digit
+fn get_max_joltage(battery_bank: &BatteryBank, num_batteries: usize) -> i64 {
+    let mut batteries: Vec<&i64> = vec![];
+    let mut last_battery_idx = 0;
+    for batteries_remaining in (0..num_batteries).rev() {
+        let battery = battery_bank[last_battery_idx..battery_bank.len() - batteries_remaining]
+            .iter()
+            .max()
+            .unwrap();
+        last_battery_idx += 1
+            + (battery_bank[last_battery_idx..]
+                .iter()
+                .position(|i| i == battery)
+                .unwrap());
+        batteries.push(battery)
     }
+
+    batteries
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(idx, battery)| *battery * 10_i64.pow(idx as u32))
+        .sum()
 }
 
 pub(crate) fn day03(lines: Vec<String>) -> FunctionOutput {
-    let battery_banks: Vec<_> = lines
+    let battery_banks: Vec<BatteryBank> = lines
         .iter()
         .map(|l| {
-            BatteryBank(
-                l.chars()
-                    .map(|c| match c.to_string().parse() {
-                        Ok(x) => x,
-                        Err(_) => panic!("Could not parse `{}`", c),
-                    })
-                    .collect(),
-            )
+            l.chars()
+                .map(|c| match c.to_string().parse() {
+                    Ok(x) => x,
+                    Err(_) => panic!("Could not parse `{}`", c),
+                })
+                .collect()
         })
         .collect();
 
-    let max_joltages: Vec<_> = battery_banks.iter().map(BatteryBank::max_joltage).collect();
+    let max_joltages_2: Vec<_> = battery_banks
+        .iter()
+        .map(|bb| get_max_joltage(bb, 2))
+        .collect();
+    let max_joltages_12: Vec<_> = battery_banks
+        .iter()
+        .map(|bb| get_max_joltage(bb, 12))
+        .collect();
 
-    IntPair(max_joltages.iter().sum(), 0)
+    IntPair(max_joltages_2.iter().sum(), max_joltages_12.iter().sum())
 }
 
 mod tests {
@@ -41,12 +60,12 @@ mod tests {
     #[test]
     fn test_day_11_small() {
         let lines = read_testfile("day03test.txt");
-        assert_eq!(day03(lines), IntPair(357, 0));
+        assert_eq!(day03(lines), IntPair(357, 3121910778619));
     }
 
     #[test]
     fn test_day_11() {
         let lines = read_testfile("day03.txt");
-        assert_eq!(day03(lines), IntPair(17244, 0));
+        assert_eq!(day03(lines), IntPair(17244, 171435596092638));
     }
 }
